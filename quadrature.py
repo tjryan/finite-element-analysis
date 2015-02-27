78bcc65cb67c88ed5a5cefc5a8c0d27f6c80bb1f
Tyler Ryan <tjryan2424@gmail.com>
2015-02-26 19:38:58 -0800

    Add implementation of QuadraturePoint class. Add methods for calculating the jacobian matrix, updating the deformation gradient, and updating the material response.

--- quadrature.py
+++ quadrature.py
@@ -3,6 +3,7 @@ quadrature.py contains Gauss quadrature tables for use in numerical integration.
 
 .. moduleauthor:: Tyler Ryan <tyler.ryan@engineering.ucla.edu>
 """
+import numpy
 
 
 class BaseQuadrature:
@@ -41,3 +42,71 @@ class GaussQuadratureThreePoint(BaseQuadrature):
     point_positions = [(1 / 6, 1 / 6), (2 / 3, 1 / 6), (1 / 6, 2 / 3)]
     point_weights = [1 / 3, 1 / 3, 1 / 3]
 
+
+class QuadraturePoint:
+    """Individual quadrature point containing material responses at a particular point in an isoparametric element."""
+    # TODO add parameters to docstring with data types, then remove tuple comment for position below
+    def __init__(self, position, weight, element):
+        self.position = position  # tuple (r,s)
+        self.weight = weight
+
+        # Calculated one time
+        self.jacobian_matrix = None
+        self.jacobian_matrix_inverse = None
+        self.calculate_jacobian_matrix(element)
+
+        # Updated every deformation
+        self.deformation_gradient = None
+        self.strain_energy_density = None
+        self.first_piola_kirchhoff_stress = None
+        self.tangent_moduli = None
+
+    def calculate_jacobian_matrix(self, element):
+        """Calculate the Jacobian matrix at the quadrature point for the provided element. This is a one time
+        calculation performed during the initialization of the quadrature point.
+
+        :param element: element object that is deformed
+        """
+        jacobian_matrix = numpy.zeros((element.degrees_of_freedom, element.degrees_of_freedom))
+        for dof in range(element.degrees_of_freedom):
+            for coordinate_index in range(element.dimension):
+                for node_index in range(element.node_quantity):
+                    jacobian_matrix[dof][coordinate_index] += (
+                        element.nodes[node_index].reference_position[dof] * element.shape_function_derivatives(
+                            node_index=node_index, r=self.position[0], s=self.position[1],
+                            coordinate_index=coordinate_index))
+        self.jacobian_matrix = jacobian_matrix
+        self.jacobian_matrix_inverse = numpy.linalg.inv(jacobian_matrix)
+
+    def update_deformation_gradient(self, element):
+        """Update the deformation gradient object for the current deformation.
+
+        :param element: element object that is deformed
+        """
+        new_deformation_gradient = numpy.zeros((element.degrees_of_freedom, element.degrees_of_freedom))
+        for dof_1 in range(element.degrees_of_freedom):
+            for dof_2 in range(element.degrees_of_freedom):
+                # Sum over nodes to interpolate value at this quadrature point
+                for node_index in range(element.node_quantity):
+                    for coordinate_index in range(element.dimension):
+                        new_deformation_gradient[dof_1][dof_2] += (
+                            element.nodes[node_index].current_position[dof_1] * element.shape_function_derivatives(
+                                node_index=node_index, position=self.position)
+                            * self.jacobian_matrix_inverse[dof_1][coordinate_index])
+        self.deformation_gradient.update_F(new_F=new_deformation_gradient,
+                                           material=element.material,
+                                           constitutive_model=element.contitutive_model,
+                                           enforce_plane_stress=element.plane_stress)
+
+    def update_material_response(self, element):
+        """Update the strain energy density, first Piola-Kirchhoff stress and tangent moduli for the current
+        configuration.
+
+        :param element: element object that is deformed
+        """
+        (self.strain_energy_density,
+         self.first_piola_kirchhoff_stress,
+         self.tangent_moduli) = element.contitutive_model.calculate_all(
+            deformation_gradient=self.deformation_gradient.F,
+            dimension=element.dimension,
+            test=True)  # TODO should this be true or false?
